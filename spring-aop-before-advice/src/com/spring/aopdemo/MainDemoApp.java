554f72f051b7854b94f9968b03d69bc888f94a61
siva-fission <siva.jonnalagadda@fissionlabs.com>
2023-02-17 16:55:35 +0530

    Realtime project implementation

--- spring-aop-before-advice/src/com/spring/aopdemo/MainDemoApp.java
+++ spring-aop-before-advice/src/com/spring/aopdemo/MainDemoApp.java
@@ -10,34 +10,22 @@ public class MainDemoApp {
 	public static void main(String[] args) {
 
 		// read spring config java class
-				AnnotationConfigApplicationContext context =
-						new AnnotationConfigApplicationContext(DemoConfig.class);
-				
-				// get the bean from spring container
-				AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
-				
-				// get membership bean from spring container
-				MembershipDAO theMembershipDAO = 
-						context.getBean("membershipDAO", MembershipDAO.class);
-						
-				// call the business method
-				theAccountDAO.addAccount();
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
 
-				// call the membership business method
-				theMembershipDAO.addSillyMember();
-								
-				// close the context
-				context.close();
+		// get the bean from spring container
+		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+
+		// get membership bean from spring container
+		MembershipDAO theMembershipDAO = context.getBean("membershipDAO", MembershipDAO.class);
+
+		// call the business method
+		theAccountDAO.addAccount();
+
+		// call the membership business method
+		theMembershipDAO.addSillyMember();
+
+		// close the context
+		context.close();
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
